9463642667812e8135adbf6dd4fddf9c0b97a994
Nibeko3 <akabekonbu@gmail.com>
2025-06-18 21:44:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ import os
 DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 
-app = FastAPI()
-
 @app.get("/test-db")
 def test_db():
     with engine.connect() as conn:
