24e21d9e2abbd8a3a8a1bbb59afce7c91d1fc8f8
Nibeko3 <akabekonbu@gmail.com>
2025-06-18 20:48:31 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "fuck u world"}
+
+from sqlalchemy import create_engine
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+app = FastAPI()
+
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as conn:
+        result = conn.execute("SELECT now()")
+        return {"db_time": list(result)[0][0]}
