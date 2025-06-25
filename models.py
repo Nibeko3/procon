b408f5f6cb01425caa56af6c0c96d4751a08dc8f
Nibeko3 <akabekonbu@gmail.com>
2025-06-25 16:05:54 +0900

    Add files via upload

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey,DateTime
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from database import Base
@@ -54,3 +54,38 @@ class Explanation(Base):
     explanation = Column(String, nullable=False)
 
     card = relationship("Card", back_populates="explanation")
+
+class Player(Base):
+    __tablename__ = "players"
+    __table_args__ = {"schema": "public"}
+
+    user_id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    score = Column(Integer, default=1500)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # 逆参照：参加したマッチ
+    matches_as_player1 = relationship("Match", back_populates="player1", foreign_keys="Match.player1_id")
+    matches_as_player2 = relationship("Match", back_populates="player2", foreign_keys="Match.player2_id")
+
+class Match(Base):
+    __tablename__ = "matches"
+    __table_args__ = {"schema": "public"}
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    player1_id = Column(Integer, ForeignKey("public.players.user_id"), nullable=False)
+    player2_id = Column(Integer, ForeignKey("public.players.user_id"), nullable=False)
+    current_turn = Column(Integer, default=1, nullable=False)
+    current_player_id = Column(Integer, ForeignKey("public.players.user_id"), nullable=False)
+
+    wallet_player1 = Column(Integer, default=100, nullable=False)
+    wallet_player2 = Column(Integer, default=100, nullable=False)
+    production_power_player1 = Column(Integer, default=200, nullable=False)
+    production_power_player2 = Column(Integer, default=200, nullable=False)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    # 参照
+    player1 = relationship("Player", foreign_keys=[player1_id], back_populates="matches_as_player1")
+    player2 = relationship("Player", foreign_keys=[player2_id], back_populates="matches_as_player2")
