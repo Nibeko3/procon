93ea5b8340fc944a3822e78c32f89b3ebe254ad7
Nibeko3 <akabekonbu@gmail.com>
2025-06-25 15:11:29 +0900

    Add files via upload

--- database.py
+++ database.py
@@ -2,6 +2,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+from database import SessionLocal  # これも正しく定義されている前提
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 # .env読み込み
 load_dotenv()
