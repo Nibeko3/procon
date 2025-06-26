c55ffafef1d588200227b896cf8e24d364c28606
Nibeko3 <akabekonbu@gmail.com>
2025-06-26 23:42:35 +0900

    Add files via upload

--- database.py
+++ database.py
@@ -10,7 +10,11 @@ load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # エンジン作成
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=3600,
+)
 
 # セッション
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
