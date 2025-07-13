d9bc912dafc5959017820e3dc74c69c901819974
Nibeko3 <akabekonbu@gmail.com>
2025-07-13 22:58:52 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ def get_effects_by_cards(db: Session = Depends(get_db)):
         .join(models.Effect, models.Card.effect_id == models.Effect.effect_id)
         .all()
     )
-    return [effect[0] for effect in effects]  
+    return [effect.effect for effect in effects]  
 
 
 
